2a7f4768fd621b3fb91edd41405e6ffd7efd1c64
hewitt205 <hewitt205@yahoo.com>
2025-04-11 17:42:35 -0500

    more updates

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     gameOn = True
     while(gameOn):
@@ -15,6 +17,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip() #display updated screen - run at end of loop
 
         #limits FPS to 60
