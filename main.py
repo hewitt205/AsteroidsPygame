ba668b693fa5d7f64a286ca185f3119f69f11517
hewitt205 <hewitt205@yahoo.com>
2025-04-11 16:40:44 -0500

    adjusted main.py, added tracking txt for full instructions for future reference

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     gameOn = True
     while(gameOn):
@@ -13,7 +15,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.dispplay.flip() #display updated screen - run at end of loop
+        pygame.display.flip() #display updated screen - run at end of loop
+
+        #limits FPS to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     pygame.init()
