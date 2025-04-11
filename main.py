891a917500375c7b19fbcd4799bd95c18051071d
hewitt205 <hewitt205@yahoo.com>
2025-04-11 16:30:23 -0500

    adjusted main.py with more straight forward implementation

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     gameOn = True
     while(gameOn):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    pygame.Surface.fill(screen, (0,0,0))
-
-    pygame.dispplay.flip() #display updated screen - run at end of loop
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        screen.fill("black")
+        pygame.dispplay.flip() #display updated screen - run at end of loop
 
 if __name__ == "__main__":
     pygame.init()
