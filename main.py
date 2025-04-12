2a7f840df397f9fe7021427d725affe71bfb708d
hewitt205 <hewitt205@yahoo.com>
2025-04-11 22:34:22 -0500

    adding asteroid classes

--- main.py
+++ main.py
@@ -3,21 +3,39 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group() # creates the groups
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable) # set the containers for the player class
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    
     gameOn = True
     while(gameOn):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt) # ship moves
+
         screen.fill("black")
-        player.draw(screen)
+
+        for d in drawable:
+            d.draw(screen)
+        
         pygame.display.flip() #display updated screen - run at end of loop
 
         #limits FPS to 60
